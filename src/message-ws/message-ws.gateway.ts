3897830620080a104d6da1ee721bb686ce307469
Antonio <antonio.ibarra99@outlook.es>
2024-10-17 13:15:50 -0700

    WebSocket Implementation

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,27 +1,44 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer() wss: Server;
-  constructor(private readonly messageWsService: MessageWsService) {}
+  constructor(
+    private readonly messageWsService: MessageWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   //Punto para manejar la conexión
-  handleConnection(client: Socket) {
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    //Verificación, en caso de que de error, no dejaremos pasarlo
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messageWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+    /* console.log({ payload }); */
     /* console.log('Cliente conectado con el id: ', client.id); */
-    this.messageWsService.registerClient(client);
 
     //Para mandarle un mensaje a todas las personas que están conectadas
     //Para eso se necesita la instancia de WebSocket Server
-    // WebSockerServer tiene la información de todos los clientes
+    // WebSocketServer tiene la información de todos los clientes
     //Se puede utilizar para notificar a todo el mundo de la siguiente forma
     //con emit emitiremos un evento llamado clients-updated y el payload puede ser cualquier cosa
     // un objeto, un arreglo, un boolean
@@ -38,5 +55,28 @@ export class MessageWsGateway
       'clients-updated',
       this.messageWsService.getConnectedClients(),
     );
+  }
+  //Espera el nombre del evento por parte del cliente a escuchar
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    //Para emitir solo a la persona que se comunico con nosotros
+    //message-from-server
+    //! Emite únicamente al cliente.
+    /* client.emit('message-from-server', {
+      fullName: 'Soy yo',
+      message: payload.message || 'no-message!!',
+    }); */
+
+    //! Emite a todos MENOS, al cliente inicial
+    /*  client.broadcast.emit('message-from-server', {
+      fullName: 'Soy yo',
+      message: payload.message || 'no-message!!',
+    }); */
+
+    //! Emite a todos incluyendo al remitente
+    this.wss.emit('message-from-server', {
+      fullName: this.messageWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message!!',
+    });
   }
 }
--- src/message-ws/message-ws.service.ts
+++ src/message-ws/message-ws.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Socket } from 'socket.io';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
 
 interface ConnectedClients {
-  [id: string]: Socket;
+  //Al connectedClients vamos a adicionarle el usuario, en vez de que apunte al socket
+  //se le puede indicar que el id apunte a un objeto
+  [id: string]: { socket: Socket; user: User };
 }
 
 @Injectable()
 export class MessageWsService {
   private connectedClients: ConnectedClients = {};
 
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
   //Cuando un cliente se conecta
-  registerClient(client: Socket) {
+  async registerClient(client: Socket, userId: string) {
     //Se llama al connectedClients con el client.id como llave y este apuntara a client(socket)
-    this.connectedClients[client.id] = client;
+    const user = await this.userRepository.findOneBy({ id: userId });
+    //Si el usuario no lo encuentra
+    if (!user) throw new Error('User not found');
+    //Si el usuario no esta activo
+    if (!user.isActive) throw new Error('User not active');
+
+    //Verificamos que no haya un cliente con el mismo id conectado
+    this.checkUserConection(user);
+
+    this.connectedClients[client.id] = { socket: client, user: user };
   }
 
   //Cuando un cliente se desconecta
@@ -26,5 +45,32 @@ export class MessageWsService {
   //En vez de mandar el lenght se mandaran los ids de los clientes conectados
   getConnectedClients(): string[] {
     return Object.keys(this.connectedClients);
+  }
+
+  /**
+   * This TypeScript function retrieves the full name of a user based on their socket ID from a list of
+   * connected clients.
+   * @param {string} socketId - The `socketId` parameter is a string that represents the unique
+   * identifier of a socket connection.
+   * @returns The function `getUserFullName` is returning the full name of the user associated with the
+   * `socketId` provided as a parameter.
+   */
+  getUserFullName(socketId: string) {
+    return this.connectedClients[socketId].user.fullName;
+  }
+
+  /**
+   * The function `checkUserConnection` disconnects any connected client with the same user ID as the
+   * provided user.
+   * @param {User} user - User
+   */
+  private checkUserConection(user: User) {
+    for (const clientId of Object.keys(this.connectedClients)) {
+      const connectedClient = this.connectedClients[clientId];
+      if (connectedClient.user.id === user.id) {
+        connectedClient.socket.disconnect();
+        break;
+      }
+    }
   }
 }
